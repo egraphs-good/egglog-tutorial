95003179c73f341ecd7396cfac1e994546462f32
default <yz489@cs.washington.edu>
2026-05-22 02:14:18 +0000

    Revamp the rust part of the tutorial with soluttions

--- case-study-linear-algebra-compiler/src/main.rs
+++ case-study-linear-algebra-compiler/src/main.rs
@@ -6,15 +6,15 @@
 
 use std::io::Read;
 
-use egglog_experimental::ast::Command;
-use egglog_experimental::scheduler::Matches;
-use egglog_experimental::{self as egglog, add_scheduler_builder, DynamicCostModel};
+use egglog_experimental::ast::{Action, Command};
+use egglog_experimental::extract::{CostModel, Extractor};
+use egglog_experimental::scheduler::{Matches, Scheduler};
 use egglog_experimental::{
+    self as egglog,
+    DynamicCostModel, TermDag,
     ast::Literal,
-    extract::CostModel,
     new_experimental_egraph,
     prelude::{exprs::*, *},
-    scheduler::Scheduler,
 };
 
 use crate::ast::{CoreBindings, Type};
@@ -45,6 +45,10 @@ fn main() {
     };
 
     // Problem  1: run defn.egg
+    //
+    // Create a new experimental EGraph with `new_experimental_egraph()` and run
+    // the egglog program from defn.egg using
+    //     `egraph.parse_and_run_program(None, program)`.
     let mut egraph = new_experimental_egraph();
     todo!("Problem 1");
 
@@ -58,6 +62,7 @@ fn main() {
     //
     //    (relation MatrixDim (String i64 i64))
     //
+    // Hint: use `egraph.parse_and_run_program(None, &format!("..."))`.
     for decl in core_bindings.declares.iter() {
         let x = &decl.var;
         if let Type::Matrix { nrows, ncols } = decl.ty {
@@ -70,29 +75,42 @@ fn main() {
     // Problem  3:
     //
     // For each variable assignment in the input program,
-    // bind the egglog variable x to its corresponding expression.
+    // bind the egglog variable to its corresponding expression.
     //
-    // We have provided [`to_egglog_expr`] function that converts a [`CoreExpr`]
-    // to an egglog expression [`egglog::ast::Expr`]
+    // We have provided [`to_egglog_expr`] in util.rs that converts a [`CoreExpr`]
+    // to an egglog AST expression [`egglog::ast::Expr`].
+    //
+    // To run a `let` command, build a
+    //     Command::Action(Action::Let(span!(), name, expr))
+    // and pass it to `egraph.run_program(vec![cmd])`.
     for bind in core_bindings.bindings.iter() {
         let var = &bind.var;
         let expr = &bind.expr;
-        
+
         todo!("Problem 3")
     }
 
     // Problem  4:
     //
     // Now we have inserted all the ASTs and definitions. We will run our rules.
-    // To start with, let's run our rules 20 times (`(run 20)`).
+    // To start with, let's run our rules 20 times with `(run 20)`:
+    //
+    //     egraph.parse_and_run_program(None, "(run 20)").unwrap();
 
     todo!("Problem 4");
 
     // Problem  5:
     //
-    // Extract the optimized program using the `DynamicCostModel` from egglog_experimental.
-    // The extracted program is a directed acyclic graph (DAG) and has type [`TermDag`]` and [`Term`].
-    // We have provided the method [`termdag_to_bindings`] to convert to [`CoreBindings`].
+    // Extract the optimized program using `DynamicCostModel` and `Extractor`.
+    //
+    // Steps:
+    //   a) Evaluate the output variable to get its (ArcSort, Value):
+    //        egraph.eval_expr(&exprs::var(&output.var))
+    //   b) Compute an extractor:
+    //        Extractor::compute_costs_from_rootsorts(Some(vec![sort]), &egraph, DynamicCostModel)
+    //   c) Extract the best term:
+    //        extractor.extract_best(&egraph, &mut termdag, value)
+    //   d) Convert to CoreBindings using `termdag_to_bindings`
     let output = core_bindings.bindings.last().unwrap();
     let bindings: CoreBindings = todo!("Problem 5");
 
@@ -101,29 +119,43 @@ fn main() {
 
     // Problem  6:
     //
-    // Break down the rules into optimization rules and analysis rules.
-    // Improve the schedule above with the more refined rulesets.
+    // Break down the rules into optimization rules and analysis rules by adding
+    // `:ruleset optimization` and `:ruleset analysis` annotations in defn.egg,
+    // and declaring the rulesets:
+    //
+    //     (ruleset optimization)
+    //     (ruleset analysis)
+    //
+    // Then replace `(run 20)` above with a schedule that:
+    //   - Saturates the analysis rules before each optimization step
+    //   - Runs the optimization rules once per iteration
+    //
+    // Hint: use `(run-schedule (repeat 20 (saturate analysis) (run optimization)))`.
+    //
+    // Also update `egglog_program()` to include your updated defn file.
+
+    // Problem  7:
+    //
+    // Fill in the blanks for the [`FirstNScheduler`] below. FirstNScheduler
+    // applies at most `n` matches of a rule in each iteration. Compared
+    // to the default scheduler, it allows the E-graph to grow more gently.
+    //
+    // Register the scheduler with the egraph:
+    //
+    //     let scheduler_id = egraph.add_scheduler(Box::new(FirstNScheduler { n: 3 }));
+    //
+    // Then use `egraph.step_rules_with_scheduler(scheduler_id, "optimization")`
+    // inside a loop to run the optimization ruleset with the scheduler.
+    //
+    // Update Problem 6's schedule to use this scheduler for optimization rules.
 }
 
 // Problem  7:
 //
-// Fill in the blanks for the [`FirstNScheduler`]. FirstNScheduler
-// applies at most `n` matches of a rule in each iteration. Compared
-// to the default scheduler, it allows the E-graph to grow more gently.
-//
-// Add this scheduler to the global scheduler list in egglog-experimental with
-//
-//     add_scheduler_builder("first-n".into(), Box::new(new_first_n_scheduler));
-//
-// Update the `run-schedule` so that the optimization ruleset uses this scheduler.
-pub fn new_first_n_scheduler(_egraph: &EGraph, exprs: &[egglog::ast::Expr]) -> Box<dyn Scheduler> {
-    assert!(exprs.len() == 1);
-    let egglog::ast::Expr::Lit(_, Literal::Int(n)) = exprs[0] else {
-        panic!("wrong arguments to first n scheduler");
-    };
-    Box::new(FirstNScheduler { n: n as usize })
-}
-
+// Implement the `filter_matches` method.
+// FirstNScheduler should apply at most `n` matches of a rule per iteration:
+//   - If there are <= n matches: apply all of them, return false (done).
+//   - If there are > n matches: apply the first n, return true (more work remains).
 #[derive(Clone)]
 struct FirstNScheduler {
     n: usize,
@@ -137,12 +169,13 @@ impl Scheduler for FirstNScheduler {
 
 // Problem  8:
 //
-// We are going to define an alternative cost model that is not "sum of node costs".
+// We are going to define an alternative cost model that assigns the *depth*
+// of an AST as its cost. An extractor using this model will prefer shallower terms.
 //
-// The cost model, named [`AstDepthCostModel`] assigns the depth of an AST as its cost,
-// so an extractor using this cost model will extract a term with the smallest depth.
+// The cost of a compound node is: max(children_costs) + enode_cost
+// The cost of a leaf (primitive) is: 0
 //
-// Use this cost model in our extractor.
+// Use this cost model in the extractor from Problem 5 instead of DynamicCostModel.
 pub struct AstDepthCostModel;
 
 pub type C = usize;
@@ -160,7 +193,7 @@ impl CostModel<C> for AstDepthCostModel {
         todo!("Problem 8")
     }
 
-    fn container_primitive(
+    fn container_cost(
         &self,
         _egraph: &EGraph,
         _sort: &egglog::ArcSort,
@@ -170,7 +203,7 @@ impl CostModel<C> for AstDepthCostModel {
         todo!("Problem 8")
     }
 
-    fn leaf_primitive(
+    fn base_value_cost(
         &self,
         _egraph: &EGraph,
         _sort: &egglog::ArcSort,
